65d8665c4298e7eeec0c554fa742908e75d8e1a5
Mohammad Banisaeid <smbl64@gmail.com>
2022-04-13 22:22:10 +0200

    Read configurations from a file

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::net::TcpListener;
-
+use zero2prod::configurations::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Cannot bind address");
+    let configuration = get_configuration().expect("Failed to read configuration.");
+
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    println!("Listening on {}", address);
+    let listener = TcpListener::bind(address).expect("Cannot bind address");
     run(listener)?.await
 }
