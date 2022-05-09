8d328bde82720c45013f7fa6ce3a40efb01029e6
Mohammad Banisaeid <smbl64@gmail.com>
2022-05-09 07:56:15 +0200

    Add a connect timeout for the DB

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
+use std::time::Duration;
 use zero2prod::configurations::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -12,8 +13,10 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let conn_string = configuration.database.connection_string();
-    let pool =
-        PgPool::connect_lazy(&conn_string.expose_secret()).expect("Failed to connect to DB.");
+    let pool = PgPoolOptions::new()
+        .connect_timeout(Duration::from_secs(2))
+        .connect_lazy(&conn_string.expose_secret())
+        .expect("Failed to connect to DB");
 
     let address = format!(
         "{}:{}",
