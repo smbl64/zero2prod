f43890f96b17d15c9ea703a88cc750633ac31439
Mohammad Banisaeid <smbl64@gmail.com>
2022-05-14 12:16:21 +0200

    Fix quickcheck and faker compat issue

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -26,16 +26,20 @@ mod tests {
     use fake::faker::internet::en::SafeEmail;
     use fake::Fake;
     use quickcheck::{Arbitrary, Gen};
+    use rand::rngs::StdRng;
+    use rand::SeedableRng;
 
     #[derive(Debug, Clone)]
     struct ValidEmailFixture(pub String);
 
     impl Arbitrary for ValidEmailFixture {
-        fn arbitrary(_g: &mut Gen) -> Self {
-            // TODO: Figure out how to use Gen with Faker!
-            //let email2 = SafeEmail().fake_with_rng(g);
-            let email = SafeEmail().fake();
-            return ValidEmailFixture(email);
+        fn arbitrary(g: &mut Gen) -> Self {
+            // We need this workaround, since quickcheck has stopped
+            // relying on rand_core since version 1.0 and Gen does not
+            // implement RngCore anymore.
+            let mut rng = StdRng::seed_from_u64(u64::arbitrary(g));
+            let email = SafeEmail().fake_with_rng(&mut rng);
+            ValidEmailFixture(email)
         }
     }
 
